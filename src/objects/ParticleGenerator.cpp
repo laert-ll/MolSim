c749aad4b9e7c5f4c3d01cfdfc021a99ce4f92dd
Ujin <ujhong02@hotmail.com>
2024-05-04 22:02:08 +0200

    New Class ParticleGenerator for Generating particles. Only declarations yet

---
+++ src/objects/ParticleGenerator.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Ujin on 04.05.2024.
+//
+
+#include "ParticleGenerator.h"
+*ParticleContainer ParticleGenerator::generateParticles(std::array<double, 3> x,std::array<double, 3> n, double h, double m, std::array<double, 3> v, double t) {
+    //TODO: Implementation for generating Particles
+};
+std::array<double, 3> calculateVelocities(*ParticleContainer container, double m, double k_B, double t) {
+    //TODO: Implementation for updating Velocities
+};
