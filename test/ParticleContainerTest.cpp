171a119b4b3752ec7a28480cfa5f0469da8c79e8
Laert Llaveshi <l.llaveshi@tum.de>
2024-05-08 16:51:09 +0200

    added more tests to particlecontainer, added setter for mass in particle

--- test/ParticleContainerTest.cpp
+++ test/ParticleContainerTest.cpp
@@ -2,14 +2,18 @@
 #include "../src/objects/Particle.h"
 #include "gtest/gtest.h"
 
-TEST(ParticleContainerTest, AddParticle) {
+TEST(ParticleContainerTest, AddParticleGetSize) {
     ParticleContainer pc;
-    Particle p;
-    pc.addParticle(p);
-    EXPECT_EQ(pc.getSize(), 1);
+    EXPECT_EQ(pc.getSize(), 0);
+
+    Particle p1;
+    Particle p2;
+    pc.addParticle(p1);
+    pc.addParticle(p2);
+    EXPECT_EQ(pc.getSize(), 2);
 }
 
-TEST(ParticleContainerTest, GetSize) {
+TEST(ParticleContainerTest, InitializePairs) {
     ParticleContainer pc;
     Particle p1;
     Particle p2;
@@ -17,5 +21,31 @@ TEST(ParticleContainerTest, GetSize) {
     pc.addParticle(p1);
     pc.addParticle(p2);
     pc.addParticle(p3);
-    EXPECT_EQ(pc.getSize(), 3);
+    pc.initializePairs();
+    size_t expected_pairs = pc.getSize() * (pc.getSize() - 1) / 2;
+    EXPECT_EQ(pc.pair_end() - pc.pair_begin(), expected_pairs);
+}
+
+TEST(ParticleContainerTest, SetVolumes) {
+    ParticleContainer pc;
+    Particle p1;
+    Particle p2;
+    Particle p3;
+    Particle p4;
+
+    p1.setM(1.0);
+    p2.setM(3.0e-6);
+    p3.setM(9.55e-4);
+    p4.setM(1.0e-14);
+    pc.addParticle(p1);
+    pc.addParticle(p2);
+    pc.addParticle(p3);
+    pc.addParticle(p4);
+    pc.setVolumes();
+
+    std::vector<double> expectedVolumes = {1.0, 0.500001, 0.500478, 0.5};
+    auto expectedVolumeIter = expectedVolumes.begin();
+    for (auto p = pc.begin(); p != pc.end(); ++p, ++expectedVolumeIter) {
+        EXPECT_NEAR(p->getVolume(), *expectedVolumeIter, 1e-6);
+    }
 }
