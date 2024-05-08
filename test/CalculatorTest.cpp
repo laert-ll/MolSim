8b7c21ab8867df1c9a193c08f8210ca427aef751
Laert Llaveshi <l.llaveshi@tum.de>
2024-05-08 15:56:04 +0200

    adjusted CalculatorTest as google test

--- test/CalculatorTest.cpp
+++ test/CalculatorTest.cpp
@@ -1,5 +1,7 @@
-#include "../src/objects/ParticleContainer.h"
+#include "../src/calculators/Calculator.h"
+#include "../src/calculators/SVCalculator.h"
 #include "../src/objects/Particle.h"
+#include "../src/objects/ParticleContainer.h"
 #include "gtest/gtest.h"
 
 TEST(CalculatorTest, Positions) {
@@ -8,5 +10,26 @@ TEST(CalculatorTest, Positions) {
     pc.addParticle(Particle({-1, -1, -1}, {0, 0, 0}, 1.0, 0.0));
     pc.initializePairs();
     pc.setVolumes();
-    EXPECT_EQ(pc.getSize(), 2);
+    auto calculator = std::make_unique<calculators::SVCalculator>();
+    double endTime = 5.0;
+    double delta_t = 0.1;
+    double currentTime = 0.0;
+    while (currentTime < endTime) {
+        calculator->calculate(pc, delta_t);
+        currentTime += delta_t;
+    }
+    std::vector<std::array<double, 3>> expectedPositions = {
+        {0.0478651,  0.0478651,  0.0478651},
+        {-0.0478651, -0.0478651, -0.0478651}
+    };
+
+    auto it = pc.begin();
+    for (size_t i = 0; i < pc.getSize(); ++i) {
+        std::array<double, 3> expectedX = expectedPositions[i];
+        std::array<double, 3> actualX = it->getX();
+        EXPECT_NEAR(expectedX[0], actualX[0], 1e-6);
+        EXPECT_NEAR(expectedX[1], actualX[1], 1e-6);
+        EXPECT_NEAR(expectedX[2], actualX[2], 1e-6);
+        it++;
+    }
 }
--- test/ParticleContainerTest.cpp
+++ test/ParticleContainerTest.cpp
@@ -8,3 +8,14 @@ TEST(ParticleContainerTest, AddParticle) {
     pc.addParticle(p);
     EXPECT_EQ(pc.getSize(), 1);
 }
+
+TEST(ParticleContainerTest, GetSize) {
+    ParticleContainer pc;
+    Particle p1;
+    Particle p2;
+    Particle p3;
+    pc.addParticle(p1);
+    pc.addParticle(p2);
+    pc.addParticle(p3);
+    EXPECT_EQ(pc.getSize(), 3);
+}
