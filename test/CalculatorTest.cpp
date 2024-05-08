dda9e3150c49ffb222bb40206daced7376243116
Laert Llaveshi <l.llaveshi@tum.de>
2024-05-08 16:58:22 +0200

    cleaning up tests

--- test/CalculatorTest.cpp
+++ test/CalculatorTest.cpp
@@ -10,26 +10,28 @@ TEST(CalculatorTest, Positions) {
     pc.addParticle(Particle({-1, -1, -1}, {0, 0, 0}, 1.0, 0.0));
     pc.initializePairs();
     pc.setVolumes();
+
     auto calculator = std::make_unique<calculators::SVCalculator>();
     double endTime = 5.0;
     double delta_t = 0.1;
     double currentTime = 0.0;
+
     while (currentTime < endTime) {
         calculator->calculate(pc, delta_t);
         currentTime += delta_t;
     }
+
     std::vector<std::array<double, 3>> expectedPositions = {
         {0.0478651,  0.0478651,  0.0478651},
         {-0.0478651, -0.0478651, -0.0478651}
     };
 
     auto it = pc.begin();
-    for (size_t i = 0; i < pc.getSize(); ++i) {
+    for (size_t i = 0; i < pc.getSize(); ++i, ++it) {
         std::array<double, 3> expectedX = expectedPositions[i];
         std::array<double, 3> actualX = it->getX();
         EXPECT_NEAR(expectedX[0], actualX[0], 1e-6);
         EXPECT_NEAR(expectedX[1], actualX[1], 1e-6);
         EXPECT_NEAR(expectedX[2], actualX[2], 1e-6);
-        it++;
     }
 }
--- test/ParticleContainerTest.cpp
+++ test/ParticleContainerTest.cpp
@@ -28,22 +28,14 @@ TEST(ParticleContainerTest, InitializePairs) {
 
 TEST(ParticleContainerTest, SetVolumes) {
     ParticleContainer pc;
-    Particle p1;
-    Particle p2;
-    Particle p3;
-    Particle p4;
-
-    p1.setM(1.0);
-    p2.setM(3.0e-6);
-    p3.setM(9.55e-4);
-    p4.setM(1.0e-14);
-    pc.addParticle(p1);
-    pc.addParticle(p2);
-    pc.addParticle(p3);
-    pc.addParticle(p4);
+    pc.addParticle(Particle({0, 0, 0}, {0, 0, 0}, 1.0, 0.0));
+    pc.addParticle(Particle({0, 0, 0}, {0, 0, 0}, 3.0e-6, 0.0));
+    pc.addParticle(Particle({0, 0, 0}, {0, 0, 0}, 9.55e-4, 0.0));
+    pc.addParticle(Particle({0, 0, 0}, {0, 0, 0}, 1.0e-14, 0.0));
     pc.setVolumes();
 
     std::vector<double> expectedVolumes = {1.0, 0.500001, 0.500478, 0.5};
+
     auto expectedVolumeIter = expectedVolumes.begin();
     for (auto p = pc.begin(); p != pc.end(); ++p, ++expectedVolumeIter) {
         EXPECT_NEAR(p->getVolume(), *expectedVolumeIter, 1e-6);
