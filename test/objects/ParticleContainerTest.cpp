fc0222119603038d0eaecd196dd372080ee9f7ad
Laert Llaveshi <l.llaveshi@tum.de>
2024-05-12 23:54:55 +0200

    expanded particle container tests

--- test/objects/ParticleContainerTest.cpp
+++ test/objects/ParticleContainerTest.cpp
@@ -13,47 +13,91 @@ TEST(ParticleContainerTest, AddParticleGetSize) {
     EXPECT_EQ(pc.getSize(), 2);
 }
 
-TEST(ParticleContainerTest, InitializePairs) {
+TEST(ParticleContainerTest, IterateThroughParticles) {
+    ParticleContainer pc_0;
+    int expected_types_0[] = {};
+    int index_0 = 0;
+    for (auto it = pc_0.begin(); it != pc_0.end(); ++it) {
+        FAIL() << "Loop should not run with 0 particles";
+    }
+
+    ParticleContainer pc_1;
+    Particle p1(1);
+    pc_1.addParticle(p1);
+
+    int expected_types_1[] = {1};
+    int index_1 = 0;
+    for (auto it = pc_1.begin(); it != pc_1.end(); ++it) {
+        EXPECT_EQ(it->getType(), expected_types_1[index_1]);
+        ++index_1;
+    }
+
+    ParticleContainer pc_2;
+    Particle p2(2);
+    pc_2.addParticle(p1);
+    pc_2.addParticle(p2);
+    
+    int expected_types_2[] = {1, 2};
+    int index_2 = 0;
+    for (auto it = pc_2.begin(); it != pc_2.end(); ++it) {
+        EXPECT_EQ(it->getType(), expected_types_2[index_2]);
+        ++index_2;
+    }
+}
+
+TEST(ParticleContainerTest, IterateThroughPairs) {
     Particle p1(1);
     Particle p2(2);
     Particle p3(3);
     Particle p4(4);
 
-    ParticleContainer pc_empty;
-    pc_empty.initializePairs();
-    EXPECT_EQ(pc_empty.pair_begin(), pc_empty.pair_end());
+    ParticleContainer pc_0;
+    pc_0.initializePairs();
+    EXPECT_EQ(pc_0.pair_begin(), pc_0.pair_end());
 
-    ParticleContainer pc_one_particle;
-    pc_one_particle.addParticle(p1);
-    pc_one_particle.initializePairs();
-    EXPECT_EQ(pc_one_particle.pair_begin(), pc_one_particle.pair_end());
+    ParticleContainer pc_1;
+    pc_1.addParticle(p1);
+    pc_1.initializePairs();
+    EXPECT_EQ(pc_1.pair_begin(), pc_1.pair_end());
 
-    ParticleContainer pc_two_particles;
-    pc_two_particles.addParticle(p1);
-    pc_two_particles.addParticle(p2);
-    pc_two_particles.initializePairs();
-    std::vector<std::pair<int, int>> expected_pairs_two_particles = {{p1.getType(), p2.getType()}};
-    auto pair_iter_two = pc_two_particles.pair_begin();
-    for (const auto& expected_pair : expected_pairs_two_particles) {
-        EXPECT_EQ(expected_pair.first, pair_iter_two->first.get().getType());
-        EXPECT_EQ(expected_pair.second, pair_iter_two->second.get().getType());
-        ++pair_iter_two;
+    ParticleContainer pc_2;
+    pc_2.addParticle(p1);
+    pc_2.addParticle(p2);
+    pc_2.initializePairs();
+    std::vector<std::pair<int, int>> expected_pairs_2 = {{1, 2}};
+    auto expected_iter_2 = expected_pairs_2.begin();
+    for (auto it = pc_2.pair_begin(); it != pc_2.pair_end(); ++it, ++expected_iter_2) {
+        EXPECT_EQ(it->first.get().getType(), expected_iter_2->first);
+        EXPECT_EQ(it->second.get().getType(), expected_iter_2->second);
     }
 
-    ParticleContainer pc_four_particles;
-    pc_four_particles.addParticle(p3);
-    pc_four_particles.addParticle(p4);
-    pc_four_particles.initializePairs();
-    std::vector<std::pair<int, int>> expected_pairs_four_particles = {{p3.getType(), p4.getType()}};
-    auto pair_iter_four = pc_four_particles.pair_begin();
-    for (const auto& expected_pair : expected_pairs_four_particles) {
-        EXPECT_EQ(expected_pair.first, pair_iter_four->first.get().getType());
-        EXPECT_EQ(expected_pair.second, pair_iter_four->second.get().getType());
-        ++pair_iter_four;
+    ParticleContainer pc_3;
+    pc_3.addParticle(p1);
+    pc_3.addParticle(p2);
+    pc_3.addParticle(p3);
+    pc_3.initializePairs();
+    std::vector<std::pair<int, int>> expected_pairs_3 = {{1, 2}, {1, 3}, {2, 3}};
+    auto expected_iter_3 = expected_pairs_3.begin();
+    for (auto it = pc_3.pair_begin(); it != pc_3.pair_end(); ++it, ++expected_iter_3) {
+        EXPECT_EQ(it->first.get().getType(), expected_iter_3->first);
+        EXPECT_EQ(it->second.get().getType(), expected_iter_3->second);
     }
 
-    size_t expected_size = pc_four_particles.getSize() * (pc_four_particles.getSize() - 1) / 2;
-    EXPECT_EQ(pc_four_particles.pair_end() - pc_four_particles.pair_begin(), expected_size);
+    ParticleContainer pc_4;
+    pc_4.addParticle(p1);
+    pc_4.addParticle(p2);
+    pc_4.addParticle(p3);
+    pc_4.addParticle(p4);
+    pc_4.initializePairs();
+    std::vector<std::pair<int, int>> expected_pairs = {{1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}};
+    auto expected_iter = expected_pairs.begin();
+    for (auto it = pc_4.pair_begin(); it != pc_4.pair_end(); ++it, ++expected_iter) {
+        EXPECT_EQ(it->first.get().getType(), expected_iter->first);
+        EXPECT_EQ(it->second.get().getType(), expected_iter->second);
+    }
+
+    size_t expected_size = pc_4.getSize() * (pc_4.getSize() - 1) / 2;
+    EXPECT_EQ(pc_4.pair_end() - pc_4.pair_begin(), expected_size);
 }
 
 TEST(ParticleContainerTest, SetVolumes) {
@@ -65,7 +109,6 @@ TEST(ParticleContainerTest, SetVolumes) {
     pc.setVolumes();
 
     std::vector<double> expectedVolumes = {1.0, 0.500001, 0.500478, 0.5};
-
     auto expectedVolumeIter = expectedVolumes.begin();
     for (auto p = pc.begin(); p != pc.end(); ++p, ++expectedVolumeIter) {
         EXPECT_NEAR(p->getVolume(), *expectedVolumeIter, 1e-6);
