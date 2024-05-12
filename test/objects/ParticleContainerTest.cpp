6dab4650499a03398b25534ec5898d708051f355
Laert Llaveshi <l.llaveshi@tum.de>
2024-05-12 23:24:52 +0200

    expanded initialize_pairs test

--- test/objects/ParticleContainerTest.cpp
+++ test/objects/ParticleContainerTest.cpp
@@ -14,16 +14,46 @@ TEST(ParticleContainerTest, AddParticleGetSize) {
 }
 
 TEST(ParticleContainerTest, InitializePairs) {
-    ParticleContainer pc;
-    Particle p1;
-    Particle p2;
-    Particle p3;
-    pc.addParticle(p1);
-    pc.addParticle(p2);
-    pc.addParticle(p3);
-    pc.initializePairs();
-    size_t expected_pairs = pc.getSize() * (pc.getSize() - 1) / 2;
-    EXPECT_EQ(pc.pair_end() - pc.pair_begin(), expected_pairs);
+    Particle p1(1);
+    Particle p2(2);
+    Particle p3(3);
+    Particle p4(4);
+
+    ParticleContainer pc_empty;
+    pc_empty.initializePairs();
+    EXPECT_EQ(pc_empty.pair_begin(), pc_empty.pair_end());
+
+    ParticleContainer pc_one_particle;
+    pc_one_particle.addParticle(p1);
+    pc_one_particle.initializePairs();
+    EXPECT_EQ(pc_one_particle.pair_begin(), pc_one_particle.pair_end());
+
+    ParticleContainer pc_two_particles;
+    pc_two_particles.addParticle(p1);
+    pc_two_particles.addParticle(p2);
+    pc_two_particles.initializePairs();
+    std::vector<std::pair<int, int>> expected_pairs_two_particles = {{p1.getType(), p2.getType()}};
+    auto pair_iter_two = pc_two_particles.pair_begin();
+    for (const auto& expected_pair : expected_pairs_two_particles) {
+        EXPECT_EQ(expected_pair.first, pair_iter_two->first.get().getType());
+        EXPECT_EQ(expected_pair.second, pair_iter_two->second.get().getType());
+        ++pair_iter_two;
+    }
+
+    ParticleContainer pc_four_particles;
+    pc_four_particles.addParticle(p3);
+    pc_four_particles.addParticle(p4);
+    pc_four_particles.initializePairs();
+    std::vector<std::pair<int, int>> expected_pairs_four_particles = {{p3.getType(), p4.getType()}};
+    auto pair_iter_four = pc_four_particles.pair_begin();
+    for (const auto& expected_pair : expected_pairs_four_particles) {
+        EXPECT_EQ(expected_pair.first, pair_iter_four->first.get().getType());
+        EXPECT_EQ(expected_pair.second, pair_iter_four->second.get().getType());
+        ++pair_iter_four;
+    }
+
+    size_t expected_size = pc_four_particles.getSize() * (pc_four_particles.getSize() - 1) / 2;
+    EXPECT_EQ(pc_four_particles.pair_end() - pc_four_particles.pair_begin(), expected_size);
 }
 
 TEST(ParticleContainerTest, SetVolumes) {
