cf263d1161e9c2a15847fbc09010239a4ae7ec98
Laert Llaveshi <l.llaveshi@tum.de>
2024-05-13 11:18:39 +0200

    added test files/templates

---
+++ test/calculators/LJCalculatorTest.cpp
@@ -0,0 +1,9 @@
+#include "../../src/calculators/Calculator.h"
+#include "../../src/calculators/LJCalculator.h"
+#include "../../src/objects/Particle.h"
+#include "../../src/objects/ParticleContainer.h"
+#include "gtest/gtest.h"
+
+TEST(LJCalculatorTest, Positions) {
+    // TODO
+}
