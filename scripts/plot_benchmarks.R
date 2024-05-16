8752d3140bfe00452aff76b8eda2100e5e1d53a1
joonkim <148557609+rlawnsiiiii@users.noreply.github.com>
2024-05-16 23:00:24 +0200

    update script

--- scripts/plot_benchmarks.R
+++ scripts/plot_benchmarks.R
@@ -9,10 +9,10 @@ time_range = end_t - start_t
 # Benchmarks
 benchmarks <- data.table(
   delta_t = rep(c(0.0100, 0.0050, 0.0010, 0.0005)),
-  time = c(0, 0, 0, 0,
-           410, 842, 4452, 10733,
-           0, 0, 0, 0,
-           147, 329, 2079, 4499),
+  time = c(4836, 12064, 45738, 88863,
+           446, 1030, 5072, 10081,
+           967, 2248, 13128, 26957,
+           107, 229, 1310, 3787),
   group = rep(c("Without Optimization", "Distance Threshold", "Parallelization", "Both"), each = 4)
 )
 
@@ -21,8 +21,12 @@ benchmarks[, iterations := time_range / delta_t]
 # Plot
 ggplot(benchmarks, aes(x = iterations, y = time, color = group, group = group)) +
   geom_line(linewidth = 1) + 
+  geom_point(size = 3) +
   scale_color_discrete(name = "Groups",
-                       breaks = c("Without Optimization", "Distance Threshold", "Parallization", "Both")) +
+                       breaks = c("Without Optimization",
+                                  "Distance Threshold",
+                                  "Parallelization",
+                                  "Both")) +
   labs(x = "Iterations", y = "Time (ms)", color = "Groups",
        title = "Effects of optimizing LJCalculator",
        subtitle = "Plotted benchmark results as average values of 5 test runs.
@@ -38,6 +42,8 @@ ggplot(benchmarks, aes(x = iterations, y = time, color = group, group = group)) 
     axis.title = element_text(size = 14),
     axis.text = element_text(size = 12),
     panel.grid.minor = element_blank() 
-  ) 
+  ) +
+  scale_x_continuous(breaks = c(0, 1000, 2500, 5000, 7500, 10000)) +
+  scale_y_continuous(breaks = c(0, 5000, 10000, 25000, 50000, 75000, 10000))
 
 
